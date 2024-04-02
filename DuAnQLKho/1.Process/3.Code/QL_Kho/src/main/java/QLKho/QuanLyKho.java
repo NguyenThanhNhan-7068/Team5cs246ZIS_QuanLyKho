9a679110280f4deec7d64ac81a5e0009b4a31a8a
gwynnn297 <phuchoang29072000@gmail.com>
2024-04-02 22:22:11 +0700

    update form QL_Kho

--- DuAnQLKho/1.Process/3.Code/QL_Kho/src/main/java/QLKho/QuanLyKho.java
+++ DuAnQLKho/1.Process/3.Code/QL_Kho/src/main/java/QLKho/QuanLyKho.java
@@ -8,12 +8,12 @@ package QLKho;
  *
  * @author MSI
  */
-public class QuanLiKhoForm extends javax.swing.JFrame {
+public class QuanLyKho extends javax.swing.JFrame {
 
     /**
      * Creates new form QuanLiKhoForm
      */
-    public QuanLiKhoForm() {
+    public QuanLyKho() {
         initComponents();
     }
 
@@ -263,20 +263,20 @@ public class QuanLiKhoForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(QuanLiKhoForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuanLyKho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(QuanLiKhoForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuanLyKho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(QuanLiKhoForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuanLyKho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(QuanLiKhoForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QuanLyKho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new QuanLiKhoForm().setVisible(true);
+                new QuanLyKho().setVisible(true);
             }
         });
     }
---
+++ DuAnQLKho/1.Process/3.Code/QL_Kho/src/test/java/QLKho/QuanLyKhoTest.java
@@ -0,0 +1,4 @@
+package QLKho;
+
+public class QuanLyKhoTest {
+}
